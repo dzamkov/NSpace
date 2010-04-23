dcdbf4d63c06cd9b69c9648c5095533dbb2b1e13
Dmitry Zamkov <dmitry94@gmail.com>
2010-04-22 21:28:30 -0400

    Commited so I can work on another computer

---
+++ NSpace/Immutable.cs
@@ -0,0 +1,22 @@
+﻿//----------------------------------------
+// Copyright (c) 2010, Dmitry Zamkov 
+// Open source under the new BSD License
+//----------------------------------------
+using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace NSpace
+{
+    /// <summary>
+    /// Interface used to mark an object as immutable. All nonprivate functions in an immutable
+    /// object will always return the same results for the same parameters unless explicitly
+    /// stated otherwise. The functions in an immutable object must therfore have their results
+    /// based on constant variables within the object and parameters.
+    /// </summary>
+    public interface IImmutable
+    {
+
+    }
+}
--- NSpace/Physics/Entity.cs
+++ NSpace/Physics/Entity.cs
@@ -9,15 +9,43 @@ namespace NSpace.Physics
 {
     /// <summary>
     /// A dynamic object capable of interaction with other
-    /// entities and possibly external systems. All effects
-    /// one entity has on another must be pushed to it, either by
-    /// calling a method or setting a member directly. The full
-    /// information about the internal state of an entity may
-    /// be lazy evaluated.
+    /// entities and possibly external systems. This may be a combination
+    /// of other entities, or the resulting state of entities after an
+    /// interaction.
     /// </summary>
-    public interface IEntity
+    public interface IEntity : IImmutable
     {
 
+    }
+
+    /// <summary>
+    /// An effect or interaction an entity has on a system.
+    /// </summary>
+    /// <typeparam name="I">The type of data given by the effects to the system.</typeparam>
+    /// <typeparam name="O">The type of data the system gives to the effect after interaction.</typeparam>
+    public interface IEffect<I, O> : IImmutable
+    {
+        /// <summary>
+        /// Gets the input data of the effect.
+        /// </summary>
+        D Input { get; }
+
+        /// <summary>
+        /// Sets the output data of the effect. This should not change the input, because this
+        /// is an immutable object.
+        /// </summary>
+        O Output { set; }
+    }
+
+    /// <summary>
+    /// An entity that produces a set of effects of a specified type.
+    /// </summary>
+    public interface IEffectEntity<D, R> : IEntity
+    {
+        /// <summary>
+        /// Gets the set of effects of the correct type this entity produces.
+        /// </summary>
+        IEnumerable<IEffect<D, R>> Effects { get; }
     }
 
     /// <summary>
