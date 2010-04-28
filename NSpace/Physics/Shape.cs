52e2392a6d840ec14cd2fcf6f24e0cccc7298b8f
Dmitry Zamkov <dmitry94@gmail.com>
2010-04-27 20:56:19 -0400

    Lots of cleanup, starting a new way of rendering

--- NSpace/Physics/Shape.cs
+++ NSpace/Physics/Shape.cs
@@ -8,16 +8,39 @@ using System.Collections.Generic;
 namespace NSpace.Physics
 {
     /// <summary>
-    /// A region, or area in space, occupied by a certain material.
+    /// A region, or area in space that is occupied by a certain material and may change over time.
     /// </summary>
     public interface IVolume
     {
+        
+    }
 
+    /// <summary>
+    /// A volume that has a very clear bound.
+    /// </summary>
+    public interface IDefiniteVolume : IVolume
+    {
+        /// <summary>
+        /// Gets a surface which bounds the volume in such a way that the inside
+        /// of the surface touches the volume.
+        /// </summary>
+        ISurface Surface { get; }
+    }
+
+    /// <summary>
+    /// A volume that is only made with one kind of material.
+    /// </summary>
+    public interface IUniformVolume : IVolume
+    {
+        /// <summary>
+        /// Gets the material that makes up the volume.
+        /// </summary>
+        IVolumeMaterial Material { get; }
     }
 
     /// <summary>
     /// Describes what substance, or material is in a volume at a particular
-    /// point.
+    /// point, it may change over time.
     /// </summary>
     public interface IVolumeMaterial
     {
@@ -25,7 +48,7 @@ namespace NSpace.Physics
     }
 
     /// <summary>
-    /// A shaped, or curved, two dimensional region in space.
+    /// A shaped, or curved, two dimensional region in space that may change over time.
     /// </summary>
     public interface ISurface
     {
@@ -33,8 +56,19 @@ namespace NSpace.Physics
     }
 
     /// <summary>
+    /// A surface that is only made with one kind of material.
+    /// </summary>
+    public interface IUniformSurface : ISurface
+    {
+        /// <summary>
+        /// Gets the material that makes the surface.
+        /// </summary>
+        ISurfaceMaterial Material { get; }
+    }
+
+    /// <summary>
     /// Describes what substance, or material is on a surface at a particular
-    /// point.
+    /// point, it may change over time.
     /// </summary>
     public interface ISurfaceMaterial
     {
---
+++ NSpace/Visual/Scene.cs
@@ -0,0 +1,57 @@
+﻿//----------------------------------------
+// Copyright (c) 2010, Dmitry Zamkov 
+// Open source under the new BSD License
+//----------------------------------------
+using System;
+using System.Collections.Generic;
+using NSpace.Physics;
+
+namespace NSpace.Visual
+{
+    /// <summary>
+    /// An object that renders a volume(which can act as a complex scene) to a
+    /// two dimensional image.
+    /// </summary>
+    public class Scene : IImmutable
+    {
+        public Scene(IVolume Volume, Section Camera)
+        {
+            this._Volume = Volume;
+            this._Camera = Camera;
+        }
+
+        /// <summary>
+        /// Gets the volume that is being rendered.
+        /// </summary>
+        public IVolume Volume
+        {
+            get
+            {
+                return this._Volume;
+            }
+        }
+
+        /// <summary>
+        /// Gets the section the camera is in. The camera will be at (0,0,0) in this section
+        /// looking towards (1,0,0).
+        /// </summary>
+        public Section Camera
+        {
+            get
+            {
+                return this._Camera;
+            }
+        }
+
+        /// <summary>
+        /// Renders the volume to the current GL graphics context.
+        /// </summary>
+        public void Render()
+        {
+
+        }
+
+        private IVolume _Volume;
+        private Section _Camera;
+    }
+}
