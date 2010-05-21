27c33a2cae5996ce859a29b74b6b0e47448c7517
Dmitry Zamkov <dmitry94@gmail.com>
2010-05-20 22:06:52 -0400

    Had idea, not enough time to implement

--- NSpaceCode/src/NSpaceCode/DebugMain.hs
+++ NSpaceCode/src/NSpaceCode/DebugMain.hs
@@ -7,9 +7,3 @@
 -----------------------------------------------------------------------------
 
 import NSpaceCode.Expression
-import NSpaceCode.Type
-
-bool2Exp x	=	Constant $ TypeValue $ BoolValue $ x
-
-testExp1		=	(Function (Constant valueNot) (bool2Exp True))
-testExp2 	=	(Function (Function (Function (Constant valueITE) (bool2Exp True)) (bool2Exp False)) (bool2Exp True))
