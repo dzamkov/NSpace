af59bed15958ff57e82ea53652cf5a39ebc88f64
Dmitry Zamkov <dmitry94@gmail.com>
2010-05-23 18:28:26 -0400

    Forall

--- NSpaceCode/src/NSpaceCode/DebugMain.hs
+++ NSpaceCode/src/NSpaceCode/DebugMain.hs
@@ -8,7 +8,7 @@
 
 import NSpaceCode.Expression
 
--- Test exp:	(a + b = c) = (a = c - b) and x + 7 = 3 + 9
+-- Test exp:	forall a, b, c (a + b = c) = (a = c - b) and x + 7 = 3 + 9
 var0	=	createVar 0 	-- a
 var1	=	createVar 1 	-- b
 var2	=	createVar 2 	-- c
@@ -22,13 +22,14 @@ var9	=	createVar 9		-- 9
 var10	=	createVar 10	-- -
 exp0	=	createFunc (createFunc var4 var0) var1			-- a + b
 exp1	=	createFunc (createFunc var3 exp0) var2			-- a + b = c
-exp2	=	createFunc (createFunc var10 var2) var1			-- c - b
+exp2	=	createFunc (createFunc var10 var2) var1		-- c - b
 exp3	=	createFunc (createFunc var3 var0) exp2			-- a = c - b
 exp4	=	createFunc (createFunc var3 exp1) exp3			-- (a + b = c) = (a = c - b)
-exp5	=	createFunc (createFunc var4 var6) var7			-- x + 7
-exp6	=	createFunc (createFunc var4 var8) var9			-- 3 + 9
-exp7	=	createFunc (createFunc var3 exp5) exp6			-- x + 7 = 3 + 9
-exp8	=	createFunc (createFunc var5 exp4) exp7			-- (a + b = c) = (a = c - b) and x + 7 = 3 + 9 
+exp5	=	createForAll [0, 1, 2] exp4						-- forall a, b, c (a + b = c) = (a = c - b)
+exp6	=	createFunc (createFunc var4 var6) var7			-- x + 7
+exp7	=	createFunc (createFunc var4 var8) var9			-- 3 + 9
+exp8	=	createFunc (createFunc var3 exp6) exp7			-- x + 7 = 3 + 9
+exp9	=	createFunc (createFunc var5 exp5) exp8			-- forall a, b, c (a + b = c) = (a = c - b) and x + 7 = 3 + 9 
 
 svs		=	SpecialVarSet 11 12 3 5 13
-prog	=	createProgram exp8 svs
+prog	=	createProgram exp9 svs
--- NSpaceCode/src/NSpaceCode/Expression.hs
+++ NSpaceCode/src/NSpaceCode/Expression.hs
@@ -9,16 +9,14 @@
 module NSpaceCode.Expression (
 	Expression,
 	createVar,
-	reallocateExpVar,
-	boundExpVars,
-	remapExpVars,
-	mapExps,
 	createFunc,
+	createForAll,
 	SpecialVarSet(..),
 	TruthSet(..),
 	createProgram
 ) where 
 
+import Data.Unique
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 
@@ -29,56 +27,13 @@ import qualified Data.Map as Map
 
 data Expression		=	
 	Variable Int |
-	Function Expression Expression deriving (Show, Ord, Eq)
+	Function Expression Expression |
+	ForAll [Int] Expression
 	
--- Creates a variable from a variable index.
+-- Creates a new variable from a variable index.
 	
-createVar		::	Int -> Expression
+createVar		:: Int -> Expression
 createVar x		=	Variable x
-
--- Reallocates the variables in an expression so that they are mapped
--- continously starting at the specified index. The resulting expression
--- and first unused variable are returned.
-
-reallocateExpVar						::	Expression -> Int -> (Expression, Int)
-reallocateExpVar (Variable x) y			=	(Variable y, y + 1)
-reallocateExpVar (Function a b) y		=	(Function (fst func) (fst arg), snd arg)
-										where
-											func	=	reallocateExpVar a y
-											arg		=	reallocateExpVar b (snd func)
-										
--- Gets a set of all variable indices that are bound in the expression.
-										
-boundExpVars					::	Expression -> (Set.Set Int)
-boundExpVars (Variable x)		=	Set.singleton x
-boundExpVars (Function x y)		=	Set.union (boundExpVars x) (boundExpVars y)
-
--- Remaps the variables in an expression based on a mapping function.
-
-remapExpVars					::	Expression -> (Int -> Int) -> Expression
-remapExpVars (Variable x) y		=	Variable (y x)
-remapExpVars (Function x y) z	=	Function (remapExpVars x z) (remapExpVars y z)
-
--- Creates a mapping between two expression by making the first expression compatible
--- with the second. The specified function shows the mapping of the first expression
--- to the second if one exists.
-
-mapExps				::	Expression -> Expression -> (Int -> Maybe Int) -> Expression
-mapExps a b x		=	remapExpVars a ((Map.!) (snd fullmap))
-					where
-						expa	=	boundExpVars a
-						expb	=	boundExpVars b
-						foldf					::	Int -> (Int, Map.Map Int Int) -> (Int, Map.Map Int Int)
-						foldf nxt (uns, map)	=	(nextun, Map.insert nxt var map)
-												where
-													findVar					::	(Maybe Int, Int) -> (Int, Int)
-													findVar ((Just n), m)	=	(n, m)
-													findVar ((Nothing), m)	=	(m, m + 1)
-													fv		=	findVar (x nxt, uns)
-													var		=	fst fv
-													nextun	=	snd fv
-						fullmap	=	(Set.fold foldf (Set.findMax expb + 1, Map.empty) expa) :: (Int, Map.Map Int Int)
-						
 
 -- Creates a function as the application of an expression on
 -- another. There should be a direct relation of variables between
@@ -86,6 +41,12 @@ mapExps a b x		=	remapExpVars a ((Map.!) (snd fullmap))
 
 createFunc			::	Expression -> Expression -> Expression
 createFunc x y		=	Function x y
+
+-- Creates a special forall expression which is true if and only if the
+-- inner expression is true for all possible values of the specified variables.
+											
+createForAll			::	[Int] -> Expression -> Expression
+createForAll x y		=	ForAll x y
 												
 -- Programs are defined by a special variable set and a true expression. A truth
 -- set contains expressions that are all true.
