6aea649e7303c8151fea03de79b4e07e7314351c
Dmitry Zamkov <dmitry94@gmail.com>
2010-05-24 17:23:37 -0400

    Some kind of progress, I assume

--- NSpaceCode/src/NSpaceCode/Expression.hs
+++ NSpaceCode/src/NSpaceCode/Expression.hs
@@ -11,11 +11,10 @@ module NSpaceCode.Expression (
 	createVar,
 	createFunc,
 	createForAll,
+	getBoundVars,
+	getDynamicVars,
 	replaceVar,
 	matchExp,
-	SpecialVarSet(..),
-	TruthSet(..),
-	createProgram
 ) where 
 
 import qualified Data.Set as Set
@@ -26,64 +25,56 @@ import qualified Data.Map as Map
 -- Expression determines the type of the value it can 
 -- evaluate to.
 
-data Expression		=	
-	Variable Int |
-	Function Expression Expression |
-	ForAll (Set.Set Int) Expression deriving (Show, Eq)
+data Expression a		=	
+	Variable a |
+	Function (Expression a) (Expression a) |
+	ForAll (Set.Set a) (Set.Set a) (Expression a) deriving (Show, Eq)
 	
--- Creates a new variable from a variable index.
+-- Creates a new variable expression.
 	
-createVar		:: Int -> Expression
+createVar		:: a -> Expression a
 createVar x		=	Variable x
 
 -- Creates a function as the application of an expression on
 -- another. There should be a direct relation of variables between
 -- the two expression.
 
-createFunc			::	Expression -> Expression -> Expression
+createFunc			::	Expression a -> Expression a -> Expression a
 createFunc x y		=	Function x y
 
 -- Creates a special forall expression which is true if and only if the
 -- inner expression is true for all possible values of the specified variables.
 											
-createForAll			:: (Set.Set Int) -> Expression -> Expression
-createForAll x y		=	ForAll x y
+createForAll			:: (Ord a) => (Set.Set a) -> (Set.Set a) -> Expression a -> Expression a
+createForAll x y z	=	ForAll x y z
 												
--- Programs are defined by a special variable set and a true expression. A truth
--- set contains expressions that are all true.
-							
-data SpecialVarSet	=	SpecialVarSet {
-	getTrueVar		::	Int,
-	getFalseVar		::	Int,
-	getEqualsVar	::	Int,
-	getAndVar		::	Int,
-	getIFTVar		::	Int } deriving (Show, Eq)
-	
-data TruthSet	=	TruthSet {
-	getTrueExpressions	::	Set.Set Expression,
-	getSpecialVarSet	::	SpecialVarSet }
-	
--- Creates a program from a true expression and special var set.
-	
-createProgram		::	Expression -> SpecialVarSet -> TruthSet
-createProgram x y	=	TruthSet (Set.singleton x) y
+-- Gets the set of variables bound by an expression.
+												
+getBoundVars						::	(Ord a) => Expression a -> (Set.Set a)
+getBoundVars (Variable x)		=	Set.singleton x
+getBoundVars (Function x y)	=	Set.union (getBoundVars x) (getBoundVars y)
+getBoundVars (ForAll x y z)	=	y
 
+-- Given a forall expression, this will return all the dynamic
+-- variables defined by it.
+
+getDynamicVars							::	(Ord a) => Expression a -> (Set.Set a) 
+getDynamicVars (ForAll x _ _)		=	x
+getDynamicVars _						=	Set.empty
+												
 -- Replaces a variable in an expression.
 
-replaceVar	::	Expression -> Int -> Int -> Expression
+replaceVar	::	(Eq a) => Expression a -> a -> a -> Expression a
 replaceVar (Variable a) b c		=	if	a == b
 												then (Variable c)
 												else (Variable a)
 replaceVar (Function a b) c d		=	Function (replaceVar a c d) (replaceVar b c d)
-replaceVar (ForAll a b) c d		=	if Set.member c a
-												then ForAll a b
-												else ForAll a (replaceVar b c d)
 
 -- Given a target expression, pattern expression and set of free variables
 -- in the pattern expression, this will determine if the target and pattern
 -- expression "match" and the values of the free variables if so.
 
-matchExp	::	Expression -> Expression -> Set.Set Int -> Maybe (Map.Map Int Expression)
+matchExp	::	(Ord a) => Expression a -> Expression a -> Set.Set a -> Maybe (Map.Map a (Expression a))
 matchExp a (Variable b) c 			= 	if Set.member b c
 												then Just (Map.singleton b a)
 												else	case a of
@@ -94,7 +85,3 @@ matchExp a (Variable b) c 			= 	if Set.member b c
 matchExp (Function a b) (Function c d) e		=	case ((matchExp a c e), (matchExp b d e)) of
 																	((Just x), (Just y))		->		Just (Map.union x y)
 																	_								-> 	Nothing
-matchExp (ForAll a b) (ForAll c d) e		=	matchExp testexp d e
-														where
-															substitution	=	zip (Set.toList a) (Set.toList c)	::	[(Int, Int)]
-															testexp			=	foldr (\x y -> replaceVar y (fst x) (snd x)) b substitution		:: Expression
