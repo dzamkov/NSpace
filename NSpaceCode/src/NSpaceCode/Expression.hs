943b66f5c5cb6e06dec8cea070699fde460de7bf
DmitryZamkov <dmitry94@gmail.com>
2010-07-27 16:54:02 -0400

    Little bit of problem solving

--- NSpaceCode/src/NSpaceCode/Expression.hs
+++ NSpaceCode/src/NSpaceCode/Expression.hs
@@ -13,8 +13,13 @@ module NSpaceCode.Expression (
 	exprMap,
 	instantiate,
 	match,
+	correspond,
+	patternMatch,
 	Rule(..),
-	Query(..)
+	Query(..),
+	QueryResult(..),
+	computeResult,
+	lambdaAxioms
 ) where 
 
 import qualified Data.Set as Set
@@ -80,30 +85,32 @@ instantiate	:: Pattern a -> Instance a -> Expression a
 instantiate p f	=	exprMap (\l -> case l of
 								(Left x)		->	f ! x
 								(Right x)	->	Term x) p
+								
+-- Finds subexpressions in the second expression that correspond to terms in
+-- the first part.
+correspond :: Expression a -> Expression b -> Maybe [(a, Expression b)]
+correspond (Term x) e									=	Just [(x, e)]
+correspond (Function al ar) (Function bl br)		=	case (correspond al bl, correspond ar br) of
+																		(Just am, Just bm) 	->	Just (am ++ bm)
+																		_							->	Nothing
+correspond (Lambda a) (Lambda b)						=	case (correspond a b) of
+																		(Just m)					->	foldl (\ac it -> case (ac, it) of
+																			(Just x, (Nothing, Term Nothing))	->	Just x
+																			(Just x, (Just y, z))					->	case (exprMaybeMap (\l -> case l of
+																				(Just m)		->	Just (Term m)
+																				(Nothing)	->	Nothing
+																																) z) of 
+																				(Just nz)									->	Just ((y, nz):x)
+																				(Nothing)									->	Nothing
+																			_												->	Nothing
+																										) (Just []) m
+																		Nothing					->	Nothing
+correspond _ _												=	Nothing
 
 -- Creates an instance from a pattern expression and a concrete expression.
 match		:: (Ord a) => Pattern a -> Expression a -> Maybe (Instance a)
 match p e	=	res
 	where
-		correspond :: Expression a -> Expression b -> Maybe [(a, Expression b)]
-		correspond (Term x) e									=	Just [(x, e)]
-		correspond (Function al ar) (Function bl br)		=	case (correspond al bl, correspond ar br) of
-																				(Just am, Just bm) 	->	Just (am ++ bm)
-																				_							->	Nothing
-		correspond (Lambda a) (Lambda b)						=	case (correspond a b) of
-																				(Just m)					->	foldl (\ac it -> case (ac, it) of
-																					(Just x, (Nothing, Term Nothing))	->	Just x
-																					(Just x, (Just y, z))					->	case (exprMaybeMap (\l -> case l of
-																						(Just m)		->	Just (Term m)
-																						(Nothing)	->	Nothing
-																																		) z) of 
-																						(Just nz)									->	Just ((y, nz):x)
-																						(Nothing)									->	Nothing
-																					_												->	Nothing
-																												) (Just []) m
-																				Nothing					->	Nothing
-		_																=	Nothing
-																				
 		res		=	case (correspond p e) of
 							(Just m)	->	foldl (\ac it -> case (ac, it) of
 								(Just a, (Left x, y))			->	case Map.lookup x a of
@@ -117,6 +124,24 @@ match p e	=	res
 								_										->	Nothing
 											) (Just (Map.empty)) m
 							Nothing	->	Nothing
+				
+-- Matches one pattern against another.				
+patternMatch	::	(Ord a) => Pattern a -> Pattern a -> Maybe (Instance (Either Int a))
+patternMatch p e	=	res
+	where
+		res		=	case (correspond p e) of 
+							(Just m) ->	foldl (\ac it -> case (ac, it) of
+								(Just a, (Left x, y))					->	case Map.lookup x a of
+									(Just z)										->	case z == y of
+										(True)										->	Just a
+										(False)										->	Nothing
+									Nothing										->	Just (Map.insert x y a)
+								(Just a, (Right x, Term (Right y)))	->	case x == y of
+									(True)										->	Just a
+									(False)										->	Nothing
+								_												->	Nothing
+											) (Just (Map.empty)) m
+							Nothing	->	Nothing
 																
 -- Describes an equivalence of patterns (as an axiom).										
 data Rule a			=	Rule (Pattern a) (Pattern a)
@@ -127,9 +152,48 @@ data Query a		=	Query (Pattern a) (Expression a)
 
 -- A result given for a query. Such a result includes all sub queries to be completed (they may be partially
 -- computed) and gives a function which takes the processed queries and computes the instances for the main query.
-data QueryResult a	=	QueryResult [Query a] ([[Instance a]] -> [Instance a])
+data QueryResult a	=	forall b. Ord b => QueryResult [Query b] ([[Instance b]] -> [Instance a])
 
-computeResult	:: Rule a -> Query a -> QueryResult a
-computeResult rules query	=	res
+computeResult	:: (Ord a) => [Rule a] -> Query a -> QueryResult a
+computeResult rules qry@(Query tarpat tarexp)	=	res
 	where
-		res	=	error "not done yet"
+	
+		-- Creates a function which can create instances of the main query by getting
+		-- the results of sub queries.
+		ruleGen	::	(Ord a) => Query a -> Pattern a -> Pattern a -> Maybe (Query a, Instance a -> Instance a)
+		ruleGen (Query tarpat tarexp) con tar	=	res
+			where
+				tarmatch	=	patternMatch tarpat tar
+				res		=	case tarmatch of
+									(Just i)	-> Just (Query con tarexp, (\inst -> Map.map (\instvar -> instantiate instvar inst) i))
+									Nothing	->	Nothing
+									
+		ruleFuncs	=	foldl (\ac it -> case (it) of
+								(Rule a b)				->	case (ruleGen qry a b, ruleGen qry b a) of
+									(Just l, Just m)		->	[l, m] ++ ac
+									(Just l, Nothing)		->	[l] ++ ac
+									(Nothing, Just m)		->	[m] ++ ac
+									(Nothing, Nothing)	->	ac
+							) [] rules
+		ruleQrs		=	map (\l -> fst l) ruleFuncs
+		ruleTrs		=	map (\l -> snd l) ruleFuncs
+		ruleFn		=	(\insts	->
+								foldl (\ac it -> case it of
+									(qreses, func)	->	foldl (\ac it -> (func it):ac) ac qreses
+								) [] (zip insts ruleTrs)
+							)
+	
+		res	=	case (tarpat, tarexp) of
+						_		->	QueryResult ruleQrs ruleFn
+				
+-- Rules true for all systems allowing lambda terms.				
+lambdaAxioms	::	[Rule a]
+lambdaAxioms	=	[
+		(Rule
+			(Function (Lambda (Term Nothing)) (Term $ Left 0))
+			(Term $ Left 0)
+		), (Rule
+			(Function (Lambda (Function (Term $ Just $ Left 1) (Term Nothing))) (Term $ Left 0))
+			(Function (Term $ Left 1) (Term $ Left 0))
+		)
+	]
