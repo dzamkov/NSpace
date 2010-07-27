02e5951e8522ff2226cd67b40ae1b95bf713d299
DmitryZamkov <dmitry94@gmail.com>
2010-07-27 13:52:14 -0400

    Changes to how expressions are represented

--- NSpaceCode/src/NSpaceCode/Expression.hs
+++ NSpaceCode/src/NSpaceCode/Expression.hs
@@ -10,13 +10,11 @@ module NSpaceCode.Expression (
 	Literal(..),
 	Expression(..),
 	Instance(..),
-	mapVar,
+	exprMap,
 	instantiate,
 	match,
 	Rule(..),
-	Query(..),
-	Solver(..),
-	quickSolve
+	Query(..)
 ) where 
 
 import qualified Data.Set as Set
@@ -45,66 +43,93 @@ data Literal	=
 	
 -- A relation between variables, and functions
 -- that produces a definite value.
-data Expression a b	=	
-	Variable a												|
-	Constant b												|
-	Function (Expression a b) (Expression a b)	|
-	Lambda (Expression a b) a							deriving (Show, Eq, Ord)
+data Expression a		=	
+	Term a												|
+	Function (Expression a) (Expression a)		|
+	Lambda (Expression (Maybe a))					deriving (Show, Eq, Ord)
 	
--- Describes a mapping of variables to expressions in an expression.
-type Instance a b	=	Map a (Expression a b)
+-- A pattern or template for an expression.
+type Pattern a			=	Expression (Either Int a)
 	
--- Maps the variables in an expression.
-mapVar	::	(a -> b) -> Expression a c -> Expression b c
-mapVar f (Variable v)	=	Variable (f v)
-mapVar f (Constant c)	=	Constant c
-mapVar f (Function l r)	=	Function (mapVar f l) (mapVar f r)
-mapVar f (Lambda e v)	=	Lambda (mapVar f e) (f v)
+-- Describes a mapping of variables to expressions in a pattern.
+type Instance a		=	Map Int (Expression a)
+
+-- Maps the terms in an expression.
+exprMap	::	(a -> Expression b) -> Expression a -> Expression b
+exprMap f (Term x)			=	(f x)
+exprMap f (Function l r)	=	Function (exprMap f l) (exprMap f r)
+exprMap f (Lambda e)			=	Lambda $ exprMap (\l -> case l of
+											(Nothing)	->	Term $ Nothing
+											(Just x)		->	exprMap (\m -> Term $ Just m) (f x)) e
+											
+exprMaybeMap	::	(a -> Maybe (Expression b)) -> Expression a -> Maybe (Expression b)
+exprMaybeMap f (Term x)			=	(f x)
+exprMaybeMap f (Function l r)	=	case (exprMaybeMap f l, exprMaybeMap f r) of
+												(Just ll, Just rr)		->	Just (Function ll rr)
+												_								->	Nothing
+exprMaybeMap f (Lambda e)		=	case exprMaybeMap (\l -> case l of
+												(Nothing)	->	Just (Term Nothing)
+												(Just x)		->	case f x of
+													(Just y)		->	Just $ exprMap (\l -> Term $ Just l) y
+													Nothing		->	Nothing) e of
+												(Just ne)	->	Just $ Lambda ne
+												(Nothing)	->	Nothing
 
 -- Given a pattern expression and an instance, creates a concrete expression.
-instantiate	::	(Ord a) => Expression a b -> Instance a b -> Expression a b
-instantiate	(Variable v) f		=	f ! v
-instantiate (Constant c) f		=	Constant c
-instantiate (Function l r) f	=	Function (instantiate l f) (instantiate r f)
-instantiate (Lambda e v) f		=	Lambda (instantiate e (Map.delete v f)) v
+instantiate	:: Pattern a -> Instance a -> Expression a
+instantiate p f	=	exprMap (\l -> case l of
+								(Left x)		->	f ! x
+								(Right x)	->	Term x) p
 
 -- Creates an instance from a pattern expression and a concrete expression.
-match		::	(Ord a, Eq b) => Expression a b -> Expression a b -> Maybe (Instance a b)
-match (Variable v) e								=	Just $ singleton v e
-match (Constant c) (Constant l)
-	|	c  == l										=	Just $ Map.empty
-	|	otherwise									=	Nothing
-match (Constant c) _								=	Nothing
-match (Function al ar) (Function bl br)	=	case (match al bl, match ar br) of
-																(Just l, Just r)	->	case Map.fold (\it ac -> it && ac) True $ Map.intersectionWith (\a b -> a == b) l r of
-																	(True)				->	Just (Map.union l r)
-																	(False)				->	Nothing
-																(_, _)				->	Nothing
-match (Function _ _) _							=	Nothing
-match (Lambda ae av) (Lambda be bv)			=	case (match ae be) of 
-																(Just l)		->	case (Map.lookup av l) of
-																	(Just m)		->	case m == Variable bv of
-																		(True)		->	Just $ Map.delete av l
-																		(False)		->	Nothing
-																	(Nothing)	->	Just l
-																Nothing		->	Nothing
-															
+match		:: (Ord a) => Pattern a -> Expression a -> Maybe (Instance a)
+match p e	=	res
+	where
+		correspond :: Expression a -> Expression b -> Maybe [(a, Expression b)]
+		correspond (Term x) e									=	Just [(x, e)]
+		correspond (Function al ar) (Function bl br)		=	case (correspond al bl, correspond ar br) of
+																				(Just am, Just bm) 	->	Just (am ++ bm)
+																				_							->	Nothing
+		correspond (Lambda a) (Lambda b)						=	case (correspond a b) of
+																				(Just m)					->	foldl (\ac it -> case (ac, it) of
+																					(Just x, (Nothing, Term Nothing))	->	Just x
+																					(Just x, (Just y, z))					->	case (exprMaybeMap (\l -> case l of
+																						(Just m)		->	Just (Term m)
+																						(Nothing)	->	Nothing
+																																		) z) of 
+																						(Just nz)									->	Just ((y, nz):x)
+																						(Nothing)									->	Nothing
+																					_												->	Nothing
+																												) (Just []) m
+																				Nothing					->	Nothing
+		_																=	Nothing
+																				
+		res		=	case (correspond p e) of
+							(Just m)	->	foldl (\ac it -> case (ac, it) of
+								(Just a, (Left x, y))			->	case Map.lookup x a of
+									(Just z)								->	case z == y of
+										(True)								->	Just a
+										(False)								->	Nothing
+									Nothing								->	Just (Map.insert x y a)
+								(Just a, (Right x, y))			->	case (Term x) == y of
+									(True)								->	Just a
+									(False)								->	Nothing
+								_										->	Nothing
+											) (Just (Map.empty)) m
+							Nothing	->	Nothing
 																
 -- Describes an equivalence of patterns (as an axiom).										
-data Rule b			=	forall a. Ord a => Rule (Expression a b) (Expression a b)
+data Rule a			=	Rule (Pattern a) (Pattern a)
 
 -- Describes a task for a solver. A query requires the solver to find some expression in terms of a pattern
 -- using a set of axioms.
-data Query a b		=	Query (Expression a b) (Expression () b)
+data Query a		=	Query (Pattern a) (Expression a)
 
--- Solves a query using a number to indicate how many rules to evaluate.
-type Solver a b	=	[Rule b] -> Query a b -> Integer -> [Instance a b]
+-- A result given for a query. Such a result includes all sub queries to be completed (they may be partially
+-- computed) and gives a function which takes the processed queries and computes the instances for the main query.
+data QueryResult a	=	QueryResult [Query a] ([[Instance a]] -> [Instance a])
 
--- Really long name for a solver.
-generalPurposeRecursiveSolver	::	Solver a b -> Solver a b
-generalPurposeRecursiveSolver solve axioms query max	=	res
+computeResult	:: Rule a -> Query a -> QueryResult a
+computeResult rules query	=	res
 	where
-		
-
--- Tye teh knot!
-quickSolve	=	generalPurposeRecursiveSolver quickSolve
+		res	=	error "not done yet"
